a46a52cbe3707581ca5ddce55c138b8b6c879e57
Dawid Michal Bieniek <bieniekdawid2aizst@gmail.com>
2015-03-26 23:23:34 +0100

    testy, nie przegladac

--- src/uncompres/Unpack.java
+++ src/uncompres/Unpack.java
@@ -1,14 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package uncompres;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 /**
  *
  * @author Dawid
  */
-public class Unpack {
-    
+public class Unpack 
+{
+    public static void main(String[] args) throws IOException
+    {
+        ZipFile zip = new ZipFile("D:\\Studia\\p.zip"); 
+        ZipEntry entry;
+        
+        Enumeration< ? extends ZipEntry> enums = zip.entries();
+        
+        while(enums.hasMoreElements())
+        {
+            entry = enums.nextElement();
+           // System.out.println(entry.getName());
+           
+        }
+    }
 }
